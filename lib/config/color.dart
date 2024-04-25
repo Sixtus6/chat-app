690852fb2e0fb4e46a2498abcbebc9b8b0df002d
Sixtus <6ixtus6@gmail.com>
2024-04-25 22:50:41 +0100

    added stacked  package to project

--- lib/config/color.dart
+++ lib/config/color.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 
 class ColorConfig {
-  static Color scaffold = Color.fromARGB(255, 0, 0, 0);
-  static Color secondary = const Color(0xFF4a4d80);
-  static Color primary = const Color(0xFF2d64f7);
-  static Color primary2 = const Color(0xFF0123db);
-  static Color primaryLight = const Color(0xFF8572e8).withOpacity(0.3);
-  static Color white = Color.fromRGBO(255, 255, 255, 1);
-  static Color carbackground = Color(0xFFd0dfee);
+  static Color buttonBorder = const Color(0xFFbbc0c5);
+  static Color iconColor = const Color(0xffDFDEDE);
+  static Color redCar = const Color(0xffe91d25);
+  static Color yellowCar = const Color(0xffe6b40e);
+  static Color greenCar = const Color(0xff0ff908);
+  static Color blueCar = const Color(0xff0523f1);
+  static Color blueButton = const Color(0xFF0376c9);
+  static Color blue = const Color(0xFF0260a4);
+  static Color containerBorder = const Color(0xFFd6d9dc);
+  static Color scaffold = const Color(0xFF001234);
+  static Color secondary = const Color(0xff034F96);
+  static Color primary = const Color(0xFF003A31);
+  static Color white = const Color(0xffffffff);
   static Color red = const Color(0xFFFF0523);
-  static Color facebook = const Color(0xFF3E74BA);
-  static Color black = Color.fromARGB(255, 0, 0, 0);
-  static Color splash = Color.fromARGB(255, 255, 255, 255);
+  static Color appBar = const Color(0xff034F96);
+  static Color desktopGameappBar = const Color(0xff034F96);
+  static Color desktopShadeGameappBar = const Color(0xff034F96);
+  static Color black = Color(0xFF24272a);
+  static Color splash = const Color(0xFF5B49CC);
   static Color dotColor = const Color(0xFFD9D9D9);
   static Color formColor = const Color(0xFF2E3850);
   static Color inputBorderColor = const Color(0xFF5B49CC);
@@ -19,10 +27,12 @@ class ColorConfig {
   static Color containerColor = const Color(0xFFE4E4E4);
   static Color seedContainer = const Color(0xFF5B49CC);
   static Color statusBarColor = const Color(0xFF111827);
-  static Color brown = const Color(0xFFB58640);
+  static Color yellow = const Color(0xFFFFC700);
   static Color tabpurple = const Color(0xFF4A3BA6);
   static Color tabcurrentindex = const Color(0xFF5B49CC);
-  static Color tabincurrentindex = const Color(0xFF999999);
+  static Color gray = const Color(0xFF999999);
   static Color coindollars = const Color(0xFF777777);
   static Color green = const Color(0xFF299927);
+  static Color lightBoarder =
+      const Color.fromARGB(255, 237, 237, 237).withOpacity(0.24);
 }
--- lib/views/splash/splash_view.dart
+++ lib/views/splash/splash_view.dart
@@ -5,27 +5,36 @@ import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:nb_utils/nb_utils.dart';
 
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({Key? key}) : super(key: key);
-
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  void flagSwitch(context) {
-    Timer(const Duration(seconds: 3), () {
-      // OnboardingScreen().launch(context,
-      //     pageRouteAnimation: PageRouteAnimation.Fade, isNewTask: true);
-    });
-  }
+class SplashScreenView extends StatelessWidget {
+  const SplashScreenView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    flagSwitch(context);
-    return Scaffold(
-      backgroundColor: ColorConfig.splash,
-      //  body: Center(child: Lottie.asset("assets/lottie/splash2.json")),
-    );
+    return const Placeholder();
   }
 }
+
+// class SplashScreen extends StatefulWidget {
+//   const SplashScreen({Key? key}) : super(key: key);
+
+//   @override
+//   State<SplashScreen> createState() => _SplashScreenState();
+// }
+
+// class _SplashScreenState extends State<SplashScreen> {
+//   void flagSwitch(context) {
+//     Timer(const Duration(seconds: 3), () {
+//       // OnboardingScreen().launch(context,
+//       //     pageRouteAnimation: PageRouteAnimation.Fade, isNewTask: true);
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     flagSwitch(context);
+//     return Scaffold(
+//       backgroundColor: ColorConfig.white,
+//       body: Center(child: Lottie.asset("assets/lottie/splash.json")),
+//     );
+//   }
+// }
