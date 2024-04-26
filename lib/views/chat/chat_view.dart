26cb8755b0f85cb294ef7145a108d7f66fe10490
Sixtus <6ixtus6@gmail.com>
2024-04-26 04:14:15 +0100

    created home view model for chat screen

--- lib/views/chat/chat_view.dart
+++ lib/views/chat/chat_view.dart
@@ -2,6 +2,9 @@ import 'package:chatapp/config/color.dart';
 import 'package:chatapp/views/chat/chat_viewmodel.dart';
 import 'package:chatapp/widgets/appBar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart';
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+import 'package:nb_utils/nb_utils.dart';
 import 'package:stacked/stacked.dart';
 
 class ChatScreenView extends StatelessWidget {
@@ -16,7 +19,31 @@ class ChatScreenView extends StatelessWidget {
               backgroundColor: ColorConfig.white,
               body: SingleChildScrollView(
                 child: Column(
-                  children: [],
+                  children: [
+                    Chat(
+                      theme: DefaultChatTheme(
+                        backgroundColor: Colors.transparent,
+                        primaryColor: ColorConfig.primary,
+                        secondaryColor: Color(0xff4d4d4d).withOpacity(0.7),
+                        // secondaryColor: Colors.white30,
+                        inputBackgroundColor: Colors.grey.shade900,
+                      ),
+                      showUserNames: true,
+                      // messages: provider.messages,
+                      //  customMessageBuilder: provider.customTextMessageBuilder,
+                      textMessageOptions:
+                          TextMessageOptions(isTextSelectable: true),
+                      messages: [],
+                      onSendPressed: (PartialText) {},
+                      user: User(id: "widget.userid", firstName: "Sixtus"),
+                      // onSendPressed: provider.handleSendPressed,
+
+                      // onSendPressed: provider.handleSendPressed,
+                      // user:
+                      //     User(id: widget.userid!, firstName: widget.firstname),
+                      // textMessageBuilder: provider.customTextMessageBuilders,
+                    ).withSize(width: double.infinity).expand()
+                  ],
                 ),
               ),
             )));
